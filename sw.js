dba21104f7eb9dca9f47521cd4576089617614eb
Losope <leo_cost@live.com>
2026-07-29 22:29:22 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "inventario-v6";
+const CACHE = "inventario-v7";
 const ARQUIVOS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
